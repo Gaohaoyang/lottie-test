17629100f50d4c08e79eb76bf780929b8f801b74
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2018-08-29 21:57:45 +0800

    t

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import lottie from 'lottie-web'
 import './index.css'
 
-lottie.loadAnimation({
-  container: document.querySelector('.img-area'),
-  renderer: 'svg',
-  name: 'myLottieAnim',
-  loop: true,
-  autoplay: true,
-  path: '/assets/water.json',
-  // path: '/assets/fingerPrint.json',
-})
+const addCtrl = () => {
+  const ctrlArea = document.querySelector('.ctrl-area')
+  const stopBtn = ctrlArea.querySelector('.stop')
+  stopBtn.addEventListener('click', () => {
+    lottie.stop('myLottieAnim')
+  })
 
-const ctrlArea = document.querySelector('.ctrl-area')
-console.log(ctrlArea)
-console.log(lottie)
+  const pauseBtn = ctrlArea.querySelector('.pause')
+  pauseBtn.addEventListener('click', () => {
+    lottie.pause('myLottieAnim')
+  })
+  const playBtn = ctrlArea.querySelector('.play')
+  playBtn.addEventListener('click', () => {
+    lottie.play('myLottieAnim')
+  })
+
+  console.log(ctrlArea)
+}
+
+const render = () => {
+  lottie.loadAnimation({
+    container: document.querySelector('.img-area'),
+    renderer: 'svg',
+    name: 'myLottieAnim',
+    loop: true,
+    autoplay: true,
+    path: '/assets/funky_chicken.json',
+    // path: '/assets/water.json',
+    // path: '/assets/fingerPrint.json',
+  })
+  console.log(lottie)
+  addCtrl()
+}
+
+render()
