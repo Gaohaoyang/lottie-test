bde6f802f885a740f119a08346c4256bfe2116e7
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2018-08-30 11:09:37 +0800

    done

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: 'airbnb-base',
   globals: {
     document: false,
+    requestAnimationFrame: false,
   },
   rules: {
     semi: ['error', 'never'],
--- src/index.js
+++ src/index.js
@@ -1,38 +1,82 @@
 import lottie from 'lottie-web'
+import Stats from 'stats.js'
+
 import './index.css'
 
-const addCtrl = () => {
+const addCtrl = (myLottie) => {
   const ctrlArea = document.querySelector('.ctrl-area')
   const stopBtn = ctrlArea.querySelector('.stop')
-  stopBtn.addEventListener('click', () => {
-    lottie.stop('myLottieAnim')
-  })
-
   const pauseBtn = ctrlArea.querySelector('.pause')
-  pauseBtn.addEventListener('click', () => {
-    lottie.pause('myLottieAnim')
-  })
   const playBtn = ctrlArea.querySelector('.play')
-  playBtn.addEventListener('click', () => {
-    lottie.play('myLottieAnim')
-  })
+  const speed2 = ctrlArea.querySelector('.speed2')
+  const speedHalf = ctrlArea.querySelector('.speed5')
+  const speed1 = ctrlArea.querySelector('.speed1')
+  const direction1 = ctrlArea.querySelector('.direction1')
+  const directionBack = ctrlArea.querySelector('.directionBack')
+  const gotoStop = ctrlArea.querySelector('.goto-stop')
+  const gotoPlay = ctrlArea.querySelector('.goto-play')
+  const segment = ctrlArea.querySelector('.segment')
 
-  console.log(ctrlArea)
+  stopBtn.addEventListener('click', () => {
+    myLottie.stop()
+  })
+  pauseBtn.addEventListener('click', () => {
+    myLottie.pause()
+  })
+  playBtn.addEventListener('click', () => {
+    myLottie.play()
+  })
+  speed2.addEventListener('click', () => {
+    myLottie.setSpeed(4)
+  })
+  speedHalf.addEventListener('click', () => {
+    myLottie.setSpeed(0.5)
+  })
+  speed1.addEventListener('click', () => {
+    myLottie.setSpeed(1)
+  })
+  direction1.addEventListener('click', () => {
+    myLottie.setDirection(1)
+  })
+  directionBack.addEventListener('click', () => {
+    myLottie.setDirection(-1)
+  })
+  gotoStop.addEventListener('click', () => {
+    myLottie.goToAndStop(1, true)
+  })
+  gotoPlay.addEventListener('click', () => {
+    myLottie.goToAndPlay(1, true)
+  })
+  segment.addEventListener('click', () => {
+    myLottie.playSegments([10, 20], true)
+  })
 }
 
 const render = () => {
-  lottie.loadAnimation({
+  const myLottie = lottie.loadAnimation({
     container: document.querySelector('.img-area'),
     renderer: 'svg',
     name: 'myLottieAnim',
     loop: true,
     autoplay: true,
-    path: './assets/funky_chicken.json',
-    // path: '/assets/water.json',
-    // path: '/assets/fingerPrint.json',
+    path: './assets/motorcycle.json',
+    // path: './assets/funky_chicken.json',
+    // path: './assets/water.json',
+    // path: './assets/fingerPrint.json',
   })
   console.log(lottie)
-  addCtrl()
+  console.log(myLottie)
+  addCtrl(myLottie)
 }
 
 render()
+
+const stats = new Stats()
+stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom)
+
+function animate() {
+  stats.update()
+  requestAnimationFrame(animate)
+}
+requestAnimationFrame(animate)
